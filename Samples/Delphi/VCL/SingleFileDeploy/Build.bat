26726de82e9e26e4edcb82bb141f69d85f8975f6
jaakkoextend <81317831+jaakkoextend@users.noreply.github.com>
2021-06-06 17:56:24 -0600

    Removed files

--- Samples/Delphi/VCL/SingleFileDeploy/Build.bat
+++
@@ -1,6 +0,0 @@
-cd Win32\Debug
-del /Q Deploy\*.*
-copy *.exe Deploy
-AddResource.exe Deploy\SingleFileDeploy.exe SingleFileDeploy.fi FI
-AddResource.exe Deploy\SingleFileDeploy.exe SingleFileDeploy.ja JA
-cd ..\..
