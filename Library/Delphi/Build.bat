fcedf9aa4406f7109293ce55de701ef21c1422d8
Jaakko Salmenius <jaakko.salmenius@gmail.com>
2023-09-17 11:13:55 +0300

    Updated samples, added support for Delphi 12

--- Library/Delphi/Build.bat
+++ Library/Delphi/Build.bat
@@ -49,6 +49,9 @@
 \Tools\DXAlexandria\Bin\dcc64 -B -JPHNE -U\Tools\Raize;\Tools\VirtualTree;\Tools\TMS -NSSystem;System.Win;WinAPI;Vcl;Vcl.Imaging;Data NtBase.pas NtLocalization.pas NtTranslator.pas NtMenu.pas NtDatabaseUtils.pas NtLanguageDlg.pas NtPictureTranslator.pas NtListViewTranslator.pas NtTreeViewTranslator.pas NtPattern.pas NtOrdinal.pas NtNumber.pas NtInitialLocale.pas NtDialog.pas NtWindows.pas NtChecker.pas NtRaizeChecker.pas NtVirtualTreeTranslator.pas NtNumber.pas NtGraphic.pas
 \Tools\DXAlexandria\Bin\dcc32 -B -JPHNE -U\Tools\Raize;\Tools\VirtualTree;\Tools\TMS -NSSystem;System.Win;WinAPI;Vcl;Vcl.Imaging;Data NtBase.pas NtLocalization.pas NtTranslator.pas NtMenu.pas NtDatabaseUtils.pas NtLanguageDlg.pas NtPictureTranslator.pas NtListViewTranslator.pas NtTreeViewTranslator.pas NtPattern.pas NtOrdinal.pas NtNumber.pas NtInitialLocale.pas NtDialog.pas NtWindows.pas NtChecker.pas NtRaizeChecker.pas NtVirtualTreeTranslator.pas NtNumber.pas NtGraphic.pas
 
+\Tools\DXYukon\Bin\dcc64 -B -JPHNE -U\Tools\Raize;\Tools\VirtualTree;\Tools\TMS -NSSystem;System.Win;WinAPI;Vcl;Vcl.Imaging;Data NtBase.pas NtLocalization.pas NtTranslator.pas NtMenu.pas NtDatabaseUtils.pas NtLanguageDlg.pas NtPictureTranslator.pas NtListViewTranslator.pas NtTreeViewTranslator.pas NtPattern.pas NtOrdinal.pas NtNumber.pas NtInitialLocale.pas NtDialog.pas NtWindows.pas NtChecker.pas NtRaizeChecker.pas NtVirtualTreeTranslator.pas NtNumber.pas NtGraphic.pas
+\Tools\DXYukon\Bin\dcc32 -B -JPHNE -U\Tools\Raize;\Tools\VirtualTree;\Tools\TMS -NSSystem;System.Win;WinAPI;Vcl;Vcl.Imaging;Data NtBase.pas NtLocalization.pas NtTranslator.pas NtMenu.pas NtDatabaseUtils.pas NtLanguageDlg.pas NtPictureTranslator.pas NtListViewTranslator.pas NtTreeViewTranslator.pas NtPattern.pas NtOrdinal.pas NtNumber.pas NtInitialLocale.pas NtDialog.pas NtWindows.pas NtChecker.pas NtRaizeChecker.pas NtVirtualTreeTranslator.pas NtNumber.pas NtGraphic.pas
+
 call BuildVclDoc.bat
 call BuildFmxDoc.bat
 
